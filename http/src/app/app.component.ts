91952ce1bbfc90fcfeb9d0c299a3f1fee66c9ad5
Antonio <shotvideo25@gmail.com>
2023-10-09 17:05:55 +0300

    delete

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { delay, pipe } from 'rxjs';
 
 interface Todo {
   completed: boolean;
   title: string;
-  id: number;
+  id?: number;
 }
 
 @Component({
@@ -16,6 +17,8 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   todos: Todo[] = [];
+  titleValue = '';
+  isLoading = false;
 
   ngOnInit() {
     this.http
@@ -23,6 +26,37 @@ export class AppComponent implements OnInit {
       .subscribe((response) => {
         this.todos = response ?? [];
       });
+  }
 
+  addTodo() {
+    this.isLoading = true;
+    console.log(this.titleValue);
+    const todo: Todo = {
+      title: this.titleValue,
+      completed: false,
+    };
+    this.http
+      .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+      .pipe(delay(2000))
+      .subscribe((response) => {
+        console.log('put: ', response);
+        this.todos.push(response);
+        this.isLoading = false;
+        this.titleValue = '';
+      });
+  }
+
+  deleteTodo(id: number) {
+    this.isLoading = true;
+    this.http
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .pipe(delay(1000))
+      .subscribe(
+        (_) => {
+          this.todos = this.todos.filter((item) => item.id != id);
+          this.isLoading = false;
+        }
+        
+      );
   }
 }
