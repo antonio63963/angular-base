5c79780d659662a9de02b30f5de474a6db94a07a
Antonio <shotvideo25@gmail.com>
2023-10-09 21:41:48 +0300

    put, errors

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -16,35 +16,45 @@ export class AppComponent implements OnInit {
   isLoading = false;
 
   ngOnInit() {
-    if (this.titleValue.trim()) {
-      this.todoService.fetchTodos().subscribe((response) => {
-        this.todos = response ?? [];
-      });
-    }
+    this.fetchTodos();
+  }
+  
+  fetchTodos() {
+    this.todoService.fetchTodos().subscribe((response) => {
+      this.todos = response ?? [];
+    });
   }
 
   addTodo() {
-    this.isLoading = true;
-    console.log(this.titleValue);
-    const todo: Todo = {
-      title: this.titleValue,
-      completed: false,
-    };
-    this.todoService.add(todo)
-      .subscribe((response) => {
+    if (this.titleValue.trim()) {
+      this.isLoading = true;
+      console.log(this.titleValue);
+      const todo: Todo = {
+        title: this.titleValue,
+        completed: false,
+      };
+      this.todoService.add(todo).subscribe((response) => {
         console.log('put: ', response);
         this.todos.push(response);
         this.isLoading = false;
         this.titleValue = '';
       });
+    }
+  }
+
+  completeTodo(id: number) {
+    this.todoService.complete(id)
+    .subscribe(response => {
+      const completed = this.todos.find(item => item.id === id);
+      completed ? completed.completed = true : false;
+    })
   }
 
   deleteTodo(id: number) {
     this.isLoading = true;
-    this.todoService.delete(id)
-      .subscribe((_) => {
-        this.todos = this.todos.filter((item) => item.id != id);
-        this.isLoading = false;
-      });
+    this.todoService.delete(id).subscribe((_) => {
+      this.todos = this.todos.filter((item) => item.id != id);
+      this.isLoading = false;
+    });
   }
 }
--- http/src/app/todos.service.ts
+++ http/src/app/todos.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay } from 'rxjs';
+import { Observable, catchError, delay, throwError } from 'rxjs';
 
 export interface Todo {
   completed: boolean;
   title: string;
   id?: number;
 }
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -16,13 +19,29 @@ export class TodosService {
   fetchTodos(): Observable<Todo[]> {
     return this.http
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .pipe(delay(500));
+      .pipe(
+        delay(500),
+        catchError((err) => {
+          console.log('Error: ', err);
+          return throwError(() => err);
+        })
+      );
   }
 
   add(todo: Todo): Observable<Todo> {
     return this.http
       .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
-      .pipe(delay(500));
+      .pipe(
+        delay(500),
+        catchError((err) => {
+          console.log('Error: ', err);
+          return throwError(() => err);
+        })
+      );
+  }
+
+  complete(id: number) {
+    return this.http.put<Todo>(`${url}/${id}`, { comleted: true });
   }
 
   delete(id: number): Observable<any> {
