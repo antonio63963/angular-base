e7d05a58109c8bef8e119610335376de5161c780
Antonio <shotvideo25@gmail.com>
2023-10-11 18:08:24 +0300

    routing links

---
+++ routing/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'about', component: AboutComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
