0bdc9c7cc7b7bf099468d3ac5b7a5b0f0a319158
Antonio <shotvideo25@gmail.com>
2023-10-17 16:49:11 +0300

    rxjs unsubscribe on other stream execute

--- rxjs-lesson/src/app/input-search/input-search.component.ts
+++ rxjs-lesson/src/app/input-search/input-search.component.ts
@@ -1,11 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {
   Observable,
+  Observer,
+  Subscriber,
   debounce,
   debounceTime,
   distinctUntilChanged,
   fromEvent,
   map,
+  takeUntil,
   tap,
 } from 'rxjs';
 
@@ -19,27 +22,39 @@ export class InputSearchComponent implements OnInit {
     | ElementRef
     | undefined;
   search$?: Observable<Event>;
+  stop$?: Observable<Event>;
 
   constructor() {}
 
   ngOnInit(): void {
     // this.search$ = new Observable((observer) => {
     //  const input = document.getElementById('inp1');
+    //  if(!input) {
+    //   observer.error('No such an element on the page!')
+    //  }
     //  input?.addEventListener('input', event => {
     //   observer.next(event);
     //  })
     // });
-    // BETTER THEN ^
 
-    this.search$ = fromEvent<Event>(document.getElementById('inp1')!, 'input')
-    
-    this.search$.pipe(
+    // BETTER THEN ^
+    this.search$ = fromEvent<Event>(document.getElementById('inp1')!, 'input');
+    this.stop$ = fromEvent<Event>(document.getElementById('stop')!, 'click');
+
+    const searchSubscribtion = this.search$.pipe(
       map((event) => (event.target as HTMLInputElement).value),
       debounceTime(500),
       map(value => value.length > 3 ? value : ''),
-      distinctUntilChanged()
+      distinctUntilChanged(),
+      takeUntil(this.stop$) // do while stop$ start works!
     ).subscribe({
       next: (value) => console.log('output: ', value)
     })
+    // subscribtion.unsubscribe()
+
+    // const stopSubscription = this.stop$.subscribe(() => {
+    //   console.log('unsub btn')
+    //   stopSubscription.unsubscribe();
+    // }) //// WOW! after subscribe we a waiting for event 'click' from button when it hapens unsubscrive executes
   }
 }
